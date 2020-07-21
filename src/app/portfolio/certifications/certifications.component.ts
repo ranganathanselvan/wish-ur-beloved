cca135d534fedc34be98c0a1eabb2569f6055fce
ranganathanselvan <ranganathanselvan@gmail.com>
2020-07-21 18:57:40 +0530

    removed service

--- src/app/portfolio/certifications/certifications.component.ts
+++ src/app/portfolio/certifications/certifications.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Certification } from 'src/app/_models/certification';
-import { CertificationService } from 'src/app/_services/portfolio-certifications.service';
 
 @Component({
   selector: 'app-portfolio-certifications',
